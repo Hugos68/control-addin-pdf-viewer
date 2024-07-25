631600fb0700ae992680589306710218a85745d2
hugos68 <hugokorteapple@gmail.com>
2024-07-25 09:08:22 +0200

    Better loading

--- js/start.js
+++ js/start.js
@@ -3,6 +3,7 @@ window.frameElement.parentElement.style.display = "flex";
 window.frameElement.parentElement.style.flexDirection = "column";
 window.frameElement.parentElement.style.flexGrow = "1";
 window.frameElement.parentElement.style.height = "100%";
+window.frameElement.parentElement.parentElement.parentElement.parentElement.style.padding = '0px';
 window.frameElement.style.removeProperty("height");
 window.frameElement.style.removeProperty("min-height");
 window.frameElement.style.removeProperty("max-height");
@@ -10,9 +11,6 @@ window.frameElement.style.flexGrow = "1";
 window.frameElement.style.flexShrink = "1";
 window.frameElement.style.flexBasis = "auto";
 window.frameElement.style.paddingBottom = "42px";
-
-// Grab root
-const root = document.querySelector("#controlAddIn");
 
 // Create iframe
 const iframe = document.createElement("iframe");
@@ -24,6 +22,9 @@ iframe.style.width = "100%";
 // Apply attributes
 iframe.title = "PDF Viewer";
 
+// Grab root
+const root = document.querySelector("#controlAddIn");
+
 // Add to root
 root.appendChild(iframe);
 
@@ -32,9 +33,14 @@ root.appendChild(iframe);
  *
  * @param {string} url - The URL of the document to load.
  */
-window.LoadDocument = (url) => {
+function loadDocument(url) {
   iframe.src = url;
-};
+}
+
+// Expose methods to the control add-in
+Object.assign(globalThis, {
+  LoadDocument: loadDocument,
+});
 
 // Notify that the control add-in is ready
 Microsoft.Dynamics.NAV.InvokeExtensibilityMethod("ready");
